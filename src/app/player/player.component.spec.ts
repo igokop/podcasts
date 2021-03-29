871da50155a12136ec03b1ffe2889b8d4abb72ce
igokop <kopaczewskii@icloud.com>
2021-03-29 23:02:20 +0200

    testing

--- src/app/player/player.component.spec.ts
+++ src/app/player/player.component.spec.ts
@@ -1,25 +1,30 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { PodcastApiService } from '../services/podcast-api.service';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { PlayerComponent } from './player.component';
 
 describe('PlayerComponent', () => {
-  let component: PlayerComponent;
-  let fixture: ComponentFixture<PlayerComponent>;
+  beforeEach(()=>{
+    TestBed.configureTestingModule({
+      declarations: [PlayerComponent],
+      providers: [PodcastApiService],
+      imports: [HttpClientTestingModule]
+    });
+  })
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PlayerComponent ]
-    })
-    .compileComponents();
-  });
+  it ('should create the component', () =>{
+    let fixture = TestBed.createComponent(PlayerComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  })
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(PlayerComponent);
-    component = fixture.componentInstance;
+  it('take value from event emitter', () =>{
+    let fixture = TestBed.createComponent(PlayerComponent);
+    let app = fixture.debugElement.componentInstance; 
+    let podcastApiService = fixture.debugElement.injector.get(PodcastApiService);
+    // let spy = spyOn(podcastApiService, 'getDataPoland');
     fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    podcastApiService.poland.subscribe(value => expect(app.trendingPoland).toBe(value));
+  })
 });
